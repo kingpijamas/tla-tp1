05831af057f8be34f919ab16c710f1d4dee094db
kpjjpk <juanpablorey.10@gmail.com>
2012-10-03 03:42:44 -0300

    Vicky: Modified the automaton definition

--- automaton.h
+++ automaton.h
@@ -1,17 +1,25 @@
 
+#define MARK 1
+#define UNMARK 0
+
+typedef state{
+	char K;
+	int mark = 0;
+}
+
 
 typedef transition{
-	char from;	//NT U {iniciales}
+	state from;	//NT U {iniciales}
 	char by;	//T U {lambda}
-	char * to;	//NT and Q_final
+	List to;	//NT and Q_final
 }transition;
 
 typedef transition * Transition;
 
 typedef automaton{
-	char * k; //conjunto de estados
+	List stateList; //conjunto de estados
 	char * sigma; //alfabeto
-	char * f; //estados finales
-	char q0; //inicial	
+	List finals; //estados finales
+	state q0; //inicial	
 	List delta; //funcion de transicion; recordar hacer la list para deltas
 }
