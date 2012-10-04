0915d835af84f9af51247a9fbbe647cc79247e98
Vicky Mesa Alcorta <m_vickymesalcorta@hotmail.com>
2012-10-04 19:52:38 -0300

    Added most of the automata methods. All untested.

--- fromDot.c
+++ fromDot.c
@@ -196,11 +196,14 @@ Grammar fromAFDtoGR(Automaton M){
 	Grammar G = malloc(sizeof(grammar));
 	G->productions = malloc(sizeof(llist));
 	char * nonTerminals = malloc(1 + M->stateList->NumEl);
-	int i,j;
-	char q,a;
+	int i,j, isFinal;
+	char q,a,r,f;
 	Transition tran;
 	Production prod;
-	Element ptr,ptr2;
+	Element ptr,ptr2,ptr3;
+
+	//0_ Cargo q0 como dist
+	G->dist = M->q0->K;
 
 	//1_ Cargo los terminales
 	addTerminal(G,M->sigma);
@@ -225,14 +228,43 @@ Grammar fromAFDtoGR(Automaton M){
 				tran = (Transition)ptr->data;
 				if(tran->from->K==q && tran->by==a){			
 					// Agrego todos los r
-					FOR_EACH(ptr2;tran->to){
+					FOR_EACH(ptr2, tran->to){
+						r = ((State)ptr->data)->K; 
 						prod = malloc(sizeof(prod));
 						prod->from = toupper(q);
-						prod->terminal = a;
-						prod->to = toupper((()ptr2->data))
+						prod->nonTerminal = toupper(r);
+						prod->terminal = tolower(a);
+						addToList(prod,G->productions);
+					
+						// Si r pertenece a finales
+						isFinal = 0;	
+						FOR_EACH(ptr3 = M->finals->pFirst; ptr3!=NULL && !isFinal; ptr3=ptr3->next){
+							f = ((State)prt3->data)->K;
+							if(f == r){
+								isFinal = 1;
+							}
+						}
+						if(isFinal){
+						prod = malloc(sizeof(prod));
+						prod->from = toupper(q);
+						prod->terminal = toupper(a);
+						addToList(prod,G->productions);
+						}
+
 					}
 				}
 			}
+		}
+	}
+
+	//4_ si q0 es final, agrego esa tran
+	FOR_EACH(ptr3, M->finals){
+		f = ((State)prt3->data)->K;
+		if(f == G->dist){
+			prod = malloc(sizeof(prod));
+			prod->from = G->dist;
+			prod->terminal = '\\';
+			addToList(prod,G->productions);
 		}
 	}
 
